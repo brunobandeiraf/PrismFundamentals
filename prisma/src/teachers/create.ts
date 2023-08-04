32e4a7b177db683d347c555810d26c4f14247b56
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-08-04 11:23:26 -0300

    Update create.ts

--- prisma/src/teachers/create.ts
+++ prisma/src/teachers/create.ts
@@ -5,7 +5,7 @@ const prisma = new PrismaClient();
 async function main() {
   const result = await prisma.teachers.create({
     data: {
-      name: "Diego Fernandes",
+      name: "Bruno Fernandes",
     },
   });
 
